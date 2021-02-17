3ff118fda2e2f55081e5685cefa39856c2cdeb23
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2021-02-17 10:10:29 +0100

    Compile script

---
+++ compile_elementary.sh
@@ -0,0 +1,13 @@
+#git clone the eos git
+git clone https://github.com/elementary/os.git && cd os
+
+
+#let's install docker and docker.io
+sudo apt install docker docker.io
+
+#now let's compile this iso
+sudo docker run --privileged -i -v /proc:/proc \
+    -v ${PWD}:/working_dir \
+    -w /working_dir \
+    debian:latest \
+    /bin/bash -s etc/terraform-daily-6.0-azure.conf < build.sh
