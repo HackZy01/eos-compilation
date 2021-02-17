0d4881f5aa1f3999d61440d18d1efb07602e2fcf
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2021-02-17 13:53:02 +0100

    Compile script

--- compile_elementary.sh
+++ compile_elementary.sh
@@ -3,7 +3,7 @@ git clone https://github.com/elementary/os.git && cd os
 
 
 #let's install docker and docker.io
-sudo apt install docker docker.io
+sudo apt-get install docker docker.io
 
 #now let's compile this iso
 sudo docker run --privileged -i -v /proc:/proc \
