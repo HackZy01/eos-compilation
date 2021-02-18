d38b788b0470a53e1c80117bb3aecc9a2482e675
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2021-02-18 08:48:21 +0100

    Bug fix 1.1

--- compile_elementary.sh
+++ compile_elementary.sh
@@ -1,9 +1,15 @@
 #git clone the eos git
 git clone https://github.com/elementary/os.git && cd os
 
-
-#let's install docker and docker.io
-sudo apt-get install docker docker.io
+#let's check if you have docker
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'docker not found, installing it.' >&2
+  sudo apt install docker 
+  
+#let's check if you have docker.io
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'docker not found, installing it.' >&2
+  sudo apt install docker.io
 
 #now let's compile this iso
 sudo docker run --privileged -i -v /proc:/proc \
