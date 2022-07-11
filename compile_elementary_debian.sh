53d874c601f6ddeac6476d64a5134373f6ce9f5a
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-07-11 21:06:26 +0200

    HUGE update
    - Fixed the version picker
    - Improved it graphically
    - made it all user friendly
    - automated almost all processes

--- compile_elementary_debian.sh
+++ compile_elementary_debian.sh
@@ -23,36 +23,67 @@ git clone https://github.com/elementary/os.git
 #go to it's directory
 cd os
 
-#BETA selection menu
-echo "select the eOS version you want to build”
-echo "  1) Daily 5.1"
-echo "  2) Daily 6.1"
-echo "  3) Daily 7.0"
-echo "  4) Stable 6.1" 
-read n
-case $n in
-  1) sudo docker run --privileged -i -v /proc:/proc \
+clear
+
+#Version selection menu
+versionselect() {
+    echo -ne "
+Select the version you want to build
+
+Daily builds:
+1) Daily 5.1 Hera
+2) Daily 6.1 Jólnir
+3) Daily 7.0 Horus
+
+Stable builds:
+4) Stable 6.1 Jólnir
+
+Choose an option:  "
+echo -e "\e[1;32mPress 5 to cancel\e[0m"
+#echo -e "\e[32mPress 5 to cancel\e[0m"
+    read -r ans
+    case $ans in
+    1)
+        sudo docker run --privileged -i -v /proc:/proc \
     -v ${PWD}:/working_dir \
     -w /working_dir \
     debian:latest \
-    /bin/bash -s etc/terraform-daily-5.1-azure.conf < build.sh;;
-  2) sudo docker run --privileged -i -v /proc:/proc \
+    /bin/bash -s etc/terraform-daily-5.1-azure.conf < build.sh
+        ;;
+    2)
+        sudo docker run --privileged -i -v /proc:/proc \
     -v ${PWD}:/working_dir \
     -w /working_dir \
     debian:latest \
-    /bin/bash -s etc/terraform-daily-6.1-azure.conf < build.sh;;
-  3) sudo docker run --privileged -i -v /proc:/proc \
+    /bin/bash -s etc/terraform-daily-6.1-azure.conf < build.sh
+        ;;
+    3)
+        sudo docker run --privileged -i -v /proc:/proc \
     -v ${PWD}:/working_dir \
     -w /working_dir \
     debian:latest \
-    /bin/bash -s etc/terraform-daily-7.0-azure.conf < build.sh;;
-  4) sudo docker run --privileged -i -v /proc:/proc \
+    /bin/bash -s etc/terraform-daily-7.0-azure.conf < build.sh
+        ;;
+    4)
+        sudo docker run --privileged -i -v /proc:/proc \
     -v ${PWD}:/working_dir \
     -w /working_dir \
     debian:latest \
-    /bin/bash -s etc/terraform-stable-azure.conf < build.sh;;
-  *) echo "invalid option";;
-esac
+    /bin/bash -s etc/terraform-stable-azure.conf < build.sh
+        ;;
+    5)
+        echo "See ya."
+        exit 0
+        ;;
+    *)
+        echo "Invalid option. Try again"
+        exit 1
+        ;;
+    esac
+}
+
+versionselect
 
 clear
-echo "done, terminal is now yours"
+exit
+
