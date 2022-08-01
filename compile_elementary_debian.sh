8753c35295a900d8808f78ac7e963109b8dce3e4
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-08-01 11:23:37 +0200

    Cleanup the script
    Remove unnecessary function descriptions

--- compile_elementary_debian.sh
+++ compile_elementary_debian.sh
@@ -2,40 +2,23 @@
 
 log=Log.txt
 
-# create log file or overrite if already present
+#Create Logs
 printf "Log File - " > $log
-
-# add date to log file
 date >> $log
-
 x=$(( 3 + 1 ))
-
-# append some data to log file
 echo value of x is $x >> $log
 
 clear
-
 echo "Generating logs to Log.txt"
 
-#let's install software-properties-common just to be sure
 sudo apt install software-properties-common -y
-
-#just to be sure fresh installed systems can use git
 sudo apt install git -y
-
-#let's install docker and docker.io
 sudo apt install docker docker.io -y
-
-#update
 sudo apt update -y && sudo apt upgrade -y
-
-#idk just it
 sudo apt --fix-broken install -y
 
-#git clone the eos git
 git clone https://github.com/elementary/os.git
 
-#go to it's directory
 cd os
 
 clear
@@ -59,7 +42,6 @@ Build for other platforms
 
 Choose an option:  "
 echo -e "\e[1;32mPress 0 to cancel\e[0m"
-#echo -e "\e[32mPress 0 to cancel\e[0m"
     read -r ans
     case $ans in
     1)
