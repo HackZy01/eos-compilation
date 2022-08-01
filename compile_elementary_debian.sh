ba06f6a358af7d1e005d905e7d2f6f88c2490495
HackZy01 <77678316+HackZy01@users.noreply.github.com>
2022-08-01 13:57:35 +0200

    Cleanup the script
    Remoce unnecessary function descriptions

--- compile_elementary_debian.sh
+++ compile_elementary_debian.sh
@@ -2,25 +2,13 @@
 
 clear
 
-#let's install software-properties-common just to be sure
 sudo apt install software-properties-common -y
-
-#just to be sure fresh installed systems can use git
 sudo apt install git -y
-
-#let's install docker and docker.io
 sudo apt install docker docker.io -y
-
-#update
 sudo apt update -y && sudo apt upgrade -y
-
-#idk just it
 sudo apt --fix-broken install -y
 
-#git clone the eos git
 git clone https://github.com/elementary/os.git
-
-#go to it's directory
 cd os
 
 clear
@@ -44,7 +32,6 @@ Build for other platforms
 
 Choose an option:  "
 echo -e "\e[1;32mPress 0 to cancel\e[0m"
-#echo -e "\e[32mPress 0 to cancel\e[0m"
     read -r ans
     case $ans in
     1)
